952f95561f53f440577ae4e5f592dc3cb21b5758
Hans <hans.larsen1994@gmail.com>
2023-07-15 02:27:19 +0200

    Removed params

--- run_subscriber.sh
+++ run_subscriber.sh
@@ -1,11 +1,5 @@
 docker run -it -v $(pwd):/workspace \
  -w /workspace \
- -v /tmp/.X11-unix:/tmp/.X11-unix \
- -v /dev/shm:/dev/shm \
- --ipc=host \
- --pid=host \
  --rm \
- --cpus=0.05 \
- --memory=0.5 \
  ros_fastdds \
  bash -c "source fastdds_ws/install/setup.bash; source ros_ws/install/setup.bash; ros2 launch test_package launch_sub.py"
